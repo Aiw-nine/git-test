c0a4942c98d00e6fb9eab8c50ccd89a83cb2b2cf
Aiw <aiw@1314.com>
2022-08-08 15:00:05 +0800

    hot-fix commit!

--- src/com/aiw/Test.java
+++ src/com/aiw/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我在IDEA中使用Git");
         System.out.println("Hello Git");
+        System.out.println("hot-fix!");
     }
 
 }
